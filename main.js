27619259f5c487c0f1a4623f71160797477758fd
codeninjah <codeninjahblog@gmail.com>
2020-10-28 14:12:29 +0100

    Main.js

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 console.log("Test")
 
+//GRUNDEN TILL ATT FÅ FRAM DATA FRÅN ETT API PÅ ETT ASYNCRONT SÄTT
 async function getStarWarsData() {
-    const req = await fetch ('https://swapi.dev/api/')
+    const req = await fetch ('https://swapi.dev/api/films')
     const res = await req.json()
 
-    console.log(res)
+    return res
+    
 }
+
+var resultPromise = getStarWarsData();
+
+resultPromise.then(function(result) {
+    console.log('got result', result);
+});
+
+//}
+
+/*
+var promise = new Promise(function(resolve, reject)
+*/
+
+/*
+promise.then(function(result) {
+    console.log("Success!", result); // "Stuff worked!"
+  }, function(err) {
+    console.log("Failed!", err); // Error: "It broke"
+  });
+  */
 
 getStarWarsData()
