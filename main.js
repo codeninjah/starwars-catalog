c86826fb47b3f5901b9f32f4a0771bd08f67b810
codeninjah <codeninjahblog@gmail.com>
2020-10-28 16:48:10 +0100

    Making progress

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
 
 //GRUNDEN TILL ATT FÅ FRAM DATA FRÅN ETT API PÅ ETT ASYNCRONT SÄTT
 async function getStarWarsData() {
-    const req = await fetch ('https://swapi.dev/api/films')
+    const req = await fetch ('https://swapi.dev/api/people')
     const res = await req.json()
 
-    return res
-    
+    return res 
 }
 
 var resultPromise = getStarWarsData();
 
 resultPromise.then(function(result) {
+    /*
     for( var r of result.results){
-        return r
+        return r.name
     }
+    */
+
+    // KARAKTÄR KOLUMNEN
+    var a = document.getElementsByClassName("character")[0]
+    a.innerHTML = "<ul>"
+
+    for(var i = 0; i < result.results.length; i++){
+        console.log(result.results[i].name)
+        a.innerHTML += "<li>" + result.results[i].name + "</li>"
+        if(i % 2 == 0) {
+            document.getElementsByTagName("li")[i].classList.add("bg-color")
+        }
+    }
+
+    a.innerHTML += "</ul>"
+    //SLUT PÅ KARAKTÄRS RUTAN
+    
     // console.log('got result', result.results);
 });
 
-console.log(getStarWarsData());
+getStarWarsData()
+resultPromise
