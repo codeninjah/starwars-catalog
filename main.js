94a894c1e3c3b6ff40ff660739fec5b4b38d7e35
renzosantamaria <renzo.santamaria@outlook.com>
2020-11-02 22:19:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -59,14 +59,8 @@ async function clickOnCharacter(charName) {
     document.querySelector(".loader-right").classList.remove("hidden")
     let charInfo = await getStarWarsData(pageNum)// väntar på info från API
 
-<<<<<<< Updated upstream
     // b.innerHTML = ""
     
-=======
-    //DETTA HAR LAGTS TILL
-    document.getElementsByClassName("character-spec")[0].innerText = ""
-
->>>>>>> Stashed changes
     for (let i = 0; i < charInfo.results.length; i++){//loopar igenom hela character listan 
         if (charName == charInfo.results[i].name) {// söker efter en match
             var b = document.getElementsByClassName("character-spec")[0]
